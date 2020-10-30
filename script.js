647963caea4c718ca726c71dba6ab21b45333659
gombosc <cosmingombos@gmail.com>
2020-10-30 20:34:26 +0200

    unable to focus on new page, needs fixing

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log("testing");
+function dowloadImages(){
+    let images = document.getElementsByTagName('img');
+    console.log(images);
+    let srcList = [];
+    let i = 0;
+
+    setInterval(function(){
+        if(images > i){
+            srcList.push(images[i].src);
+            let link = document.createElement('a');
+            link.id = i;
+            link.downloand = images[i].src;
+            link.href = images[i].src;
+            link.click();
+            i++;
+        }
+    },2000);
+}
+
+// on click opens new tab to inserted url
+// gets all images and downloads them
+
+
+button = document.getElementById('button');
+function buttonPress(){
+    input = document.getElementById('input');
+    url = input.value;
+    console.log(url);
+    window.location.replace(url);
+    setTimeout(console.log("hiii"),4000);
+    // let a = window.open(url,"name");
+    // a.focus();
+    if(confirm('Start download?')){
+        alert('Downloading');
+    }
+    else{
+        alert('Why you no love me...');
+        window.close;
+    }
+}
+button.addEventListener("click",buttonPress)
+
+
